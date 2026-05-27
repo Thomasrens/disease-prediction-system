72cdd54511549d987594c581c321d1ed97a188cd
thomasrens <thomas.rens04@gmail.com>
2026-05-27 18:12:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -44,14 +44,14 @@ if selected_disease == "Diabetes Prediction":
 
     st.divider()
 
-    pregnancies = st.number_input("Pregnancies", min_value=0.0)
+    pregnancies = st.number_input("Pregnancies", min_value=0, step=1)
     glucose = st.number_input("Glucose Level", min_value=0.0)
     blood_pressure = st.number_input("Blood Pressure", min_value=0.0)
     skin_thickness = st.number_input("Skin Thickness", min_value=0.0)
     insulin = st.number_input("Insulin Level", min_value=0.0)
     bmi = st.number_input("BMI", min_value=0.0)
     dpf = st.number_input("Diabetes Pedigree Function", min_value=0.0)
-    age = st.number_input("Age", min_value=0.0)
+    age = st.number_input("Age", min_value=0, step=1)
 
     if st.button("Predict Diabetes"):
 
